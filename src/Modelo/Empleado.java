84e67a85db52246067a9c263c4f50bb095409e0a
jeffaristi92 <jeff.aristi@gmail.com>
2015-02-11 05:59:29 -0500

    Archivos proyecto

---
+++ src/Modelo/Empleado.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author ARISTIZABAL
+ */
+public class Empleado {
+    public String Nombre;
+    public String Apellido;
+    public String Identificacion;
+    public String Cargo;
+    public double SalarioBasico;
+    public String Banco;
+    public String Cuenta;
+    public String Pension;
+    public String Salud;
+}
---
+++ src/Modelo/Item.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author ARISTIZABAL
+ */
+public class Item {
+    public enum TipoItem {Devengado, Deduccion};
+    public int Tipo;
+    public String Descripcion;
+    public int Cantidad;
+    public double Valor;
+}
